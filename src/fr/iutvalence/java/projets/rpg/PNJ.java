c628378d25dde40e698d2da69ec11204853022e0
goncalvs <goncalvs@PC-B102-04.iut-valence.fr>
2012-09-26 15:15:48 +0200

    Definition du pnj avec ces createur et suppresion du println de afficheposheros

--- src/fr/iutvalence/java/projets/rpg/PNJ.java
+++ src/fr/iutvalence/java/projets/rpg/PNJ.java
@@ -12,5 +12,49 @@ package fr.iutvalence.java.projets.rpg;
  *
  */
 public class PNJ {
-
+	
+	/**
+	 * Attributs
+	 */
+	public int pos_x_pnj;
+	
+	/**
+	 * 
+	 */
+	public int pos_y_pnj;
+	
+	
+	/**
+	 * Les paroles que vous dira le pnj lorsque le hero le solicitera
+	 */
+	public String phrase;
+	
+	
+	
+	/**
+	 * Constructeur
+	 * 
+	 * Pose un pnj a cote du hero qui lui dira coucou
+	 */
+	public PNJ(){
+		this.pos_x_pnj=9;
+		this.pos_y_pnj=5;
+		this.phrase="Hello World";
+		
+	}
+	
+	/**
+	 * 
+	 * @param pos_x Abscisse x du pnj
+	 * @param pos_y Ordonnes y du pnj
+	 * @param ph Phrase qu'il se gardera de dire
+	 */
+	public PNJ(int pos_x, int pos_y, String ph){
+		this.pos_x_pnj=pos_x;
+		this.pos_y_pnj=pos_y;
+		this.phrase=ph;
+	}
+	
+	
+	
 }
