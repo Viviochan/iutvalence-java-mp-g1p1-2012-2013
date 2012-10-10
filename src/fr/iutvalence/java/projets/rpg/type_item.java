e497605f3dc023668cf0a39bd7a721df7ca86b1b
goncalvs <goncalvs@PC-B102-06.iut-valence.fr>
2012-10-10 13:18:32 +0200

    type_item-> definition des type a enumerer et commentaire simple pour chaque
    Attention: quand passer sur le githud files la class enum retourne
    une erreur

---
+++ src/fr/iutvalence/java/projets/rpg/type_item.java
@@ -0,0 +1,57 @@
+package fr.iutvalence.java.projets.rpg;
+
+/**
+ * @author goncalvs
+ * 
+ * Type enum qui  nous servira a differencier chaque item
+ * Et la gestion de ceux-ci se fera en fonction de ce type
+ *
+ */
+public enum type_item {
+	/**
+	 * Definie le type d'objet: soin
+	 * Augmente/Redonne les hp de la valeur bonus_item
+	 */
+	objet_de_soin,
+	/**
+	 * Definie le type d'objet: recup
+	 * Augmente/Redonne les mp de la valeur bonus_item
+	 */
+	objet_de_recup,
+	/**
+	 * Definie le type d'objet: soutien
+	 * Augmente/Redonne les ' ' de la valeur bonus_item
+	 */
+	objet_de_soutien,
+	/**
+	 *Definie le type d'objet: equipement poru la tete
+	 * Augmente la defense du hero de la valeur bonus_item 
+	 */
+	equipement_tete,
+	/**
+	 * Definie le type d'objet: equipement pour le bras
+	 * Augmente la defense du hero de la valeur bonus_item
+	 */
+	equipement_bras,
+	/**
+	 * Definie le type d'objet: equipement pour le corps/buste
+	 * Augmente la defense du hero de la valeur bonus_item
+	 */
+	equipement_corps,
+	/**
+	 * Definie le type d'objet: equipement pour le pied
+	 * Augmente la defense du hero de la valeur bonus_item
+	 */
+	equipement_pied,
+	/**
+	 * Definie le type d'objet: arme
+	 * Augmente l'attaque du hero de la valeur bonus_item
+	 */
+	arme,
+	/**
+	 * Definie le type d'objet: bouclier
+	 * Augmente la defense du hero de la valeur bonus_item
+	 */
+	bouclier;
+
+}
